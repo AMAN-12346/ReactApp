8528ad56cc635cf6780d31afc107386d3c61b14c
Aman <amansinghfoujdar001@gmail.com>
2023-02-05 21:10:51 +0530

    message

--- src/alert.js
+++ src/alert.js
@@ -13,5 +13,6 @@ function Football() {
     <button onClick={(e) => shoot("Goal!")}>Take the shot!</button>
   );
 }
-
 export default Football;
+
+git remote add origin https://github.com/AMAN-12346/ReactApp.git
